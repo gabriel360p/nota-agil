17813a45cec42e136c6ac469eeb80ab85c9babc2
lorium17 <109006007+gabriel360p@users.noreply.github.com>
2023-11-20 13:19:30 -0300

    first commit

---
+++ vue-nota-agil/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="text-center">
+    <router-link to="/">Login</router-link> |
+    <router-link to="/cadastro">Cadastro</router-link>
+    <router-link to="/sobre">Sobre</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ vue-nota-agil/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/auth/LoginView.vue')
+  },
+  {
+    path: '/cadastro',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/auth/CadastroView.vue')
+  },
+  {
+    path: '/sobre',
+    name: 'sobre',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
