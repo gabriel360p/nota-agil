e4d105975ddbda75c78b8a9a242596ae140367b6
lorium17 <109006007+gabriel360p@users.noreply.github.com>
2023-11-21 15:21:50 -0300

    correções

--- vue-nota-agil/src/components/AuthBar.vue
+++ vue-nota-agil/src/components/AuthBar.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="text-center">
         <router-link to="/">Login</router-link> |
-        <router-link to="/cadastro">Cadastro</router-link> |
-        <router-link to="/sobre">Sobre</router-link>
+        <router-link to="/cadastro">Cadastro</router-link> 
+        <!-- | <router-link to="/sobre">Sobre</router-link> -->
     </nav>
     <!-- <router-view /> -->
 </template>
