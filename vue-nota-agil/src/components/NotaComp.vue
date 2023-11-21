2ad20685686fc7f1dc6718e99e52ad6a8e411c2a
lorium17 <109006007+gabriel360p@users.noreply.github.com>
2023-11-21 11:26:20 -0300

    atualização

--- vue-nota-agil/src/components/NotaComp.vue
+++ vue-nota-agil/src/components/NotaComp.vue
@@ -1,37 +1,18 @@
 <template>
     <div>
-        <div v-if="loading">Carregando...</div>
-        <div v-else>
-            {{ notas }}
-        </div>
+        <span class="border-bottom">{{ nota.id }} </span> |
+        <span class="border-bottom">{{ nota.name }} </span>
+        <br>
     </div>
 </template>
 
 <script>
-import notas from "../services/notas.js"
 
 export default {
     name: 'NotaComp',
-    data() {
-        return {
-            notas: {},
-            loading: true
-        }
-    },
 
-    methods: {
-        async fetchNotas() {
-            await notas.listar_notas().then(res => {
-                this.notas = res.data
-                this.loading = false
-            }).catch(e => {
-                alert(e)
-            })
-        }
-    },
-
-    mounted() {
-        this.fetchNotas()
+    props: {
+        nota: {},
     },
 
 }
